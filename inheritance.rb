2ef4655c5cf6f85050379281fb745c0192b8d75c
Fernando Serapio <fhernandosc@gmail.com>
2013-02-12 12:44:55 +0000

    added inheritance and mixins files practices

---
+++ inheritance.rb
@@ -0,0 +1,16 @@
+class Item
+	attr_reader :name
+
+	def initialize name, special_attributes = []
+		@id, @name = 1, name
+		@special_attributes special_attributes
+	end
+
+end
+
+#class SpecialItem < Item
+#	def initialize name, special_attributes
+#		super name
+#		@special_attributes = special_attributes
+#	end
+#end
